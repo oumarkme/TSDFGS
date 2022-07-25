1f66fbc5e7e49309c47f7791bf874f42495096f5
Jen-Hsiang Ou <oumark.me@outlook.com>
2022-07-25 23:25:32 +0200

    renew criteria
    PEV and CD criteria linked to STPGA package developed by Akdemir

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,30 +11,6 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
-// cd_score
-float cd_score(Eigen::MatrixXd X, Eigen::MatrixXd X0);
-RcppExport SEXP _TSDFGS_cd_score(SEXP XSEXP, SEXP X0SEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< Eigen::MatrixXd >::type X(XSEXP);
-    Rcpp::traits::input_parameter< Eigen::MatrixXd >::type X0(X0SEXP);
-    rcpp_result_gen = Rcpp::wrap(cd_score(X, X0));
-    return rcpp_result_gen;
-END_RCPP
-}
-// pev_score
-float pev_score(Eigen::MatrixXd X, Eigen::MatrixXd X0);
-RcppExport SEXP _TSDFGS_pev_score(SEXP XSEXP, SEXP X0SEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< Eigen::MatrixXd >::type X(XSEXP);
-    Rcpp::traits::input_parameter< Eigen::MatrixXd >::type X0(X0SEXP);
-    rcpp_result_gen = Rcpp::wrap(pev_score(X, X0));
-    return rcpp_result_gen;
-END_RCPP
-}
 // r_score
 float r_score(Eigen::MatrixXd X, Eigen::MatrixXd X0, float lambda);
 RcppExport SEXP _TSDFGS_r_score(SEXP XSEXP, SEXP X0SEXP, SEXP lambdaSEXP) {
@@ -50,8 +26,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_TSDFGS_cd_score", (DL_FUNC) &_TSDFGS_cd_score, 2},
-    {"_TSDFGS_pev_score", (DL_FUNC) &_TSDFGS_pev_score, 2},
     {"_TSDFGS_r_score", (DL_FUNC) &_TSDFGS_r_score, 3},
     {NULL, NULL, 0}
 };
