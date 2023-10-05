0e0468a1b5f160b58b0b52778ee4094b83acdb26
Jen-Hsiang Ou <oumark.me@outlook.com>
2023-10-05 13:01:55 +0200

    update link file and github action

--- R/TSDFGS.R
+++ R/TSDFGS.R
@@ -1,9 +1,11 @@
 ## usethis namespace: start
 #' TSDFGS
 #' @description We propose an optimality criterion to determine the required training set, r-score, which is derived directly from Pearson's correlation between the genomic estimated breeding values and phenotypic values of the test set <doi:10.1007/s00122-019-03387-0>. This package provides two main functions to determine a good training set and its size.
-#' @importFrom Rcpp sourceCpp
+#' @importFrom Rcpp evalCpp
+#' @import RcppEigen
 #' @docType package
 #' @useDynLib TSDFGS, .registration = TRUE
 #' @name TSDFGS
 ## usethis namespace: end
 NULL
+#Rcpp sourceCpp
